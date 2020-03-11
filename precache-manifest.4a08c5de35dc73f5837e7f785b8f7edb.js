138244a6ec51fc9d54021186e3dee86b989c897c
cityscene <wxy9326@gmail.com>
2020-03-11 16:42:23 +0800

    init.

---
+++ precache-manifest.4a08c5de35dc73f5837e7f785b8f7edb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f813ccb314700bfced7068ab24e59f07",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c50e18796816a0af2699",
+    "url": "./static/css/2.e15ada95.chunk.css"
+  },
+  {
+    "revision": "f76ee4a8fb037c01d83e",
+    "url": "./static/css/main.f4760b31.chunk.css"
+  },
+  {
+    "revision": "c50e18796816a0af2699",
+    "url": "./static/js/2.5c19ec13.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "./static/js/2.5c19ec13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76ee4a8fb037c01d83e",
+    "url": "./static/js/main.b40da4a6.chunk.js"
+  },
+  {
+    "revision": "4612178c3ac1cc535a6e",
+    "url": "./static/js/runtime-main.3083ee2d.js"
+  },
+  {
+    "revision": "7133a8167a41fb970cbc3c7bfe192113",
+    "url": "./static/media/BronzeMedal.7133a816.png"
+  },
+  {
+    "revision": "177b8e9b02bfc8b3db8b88f81c92d9db",
+    "url": "./static/media/GoldMedal.177b8e9b.png"
+  },
+  {
+    "revision": "fec3e9a6cecac7632865b2d9d4c4d36f",
+    "url": "./static/media/SilverMedal.fec3e9a6.png"
+  },
+  {
+    "revision": "9928a7f734afdca17da6bdc27bc3ed15",
+    "url": "./static/media/bernard.9928a7f7.png"
+  },
+  {
+    "revision": "ba7ff105dd369bb0b4901e160a51e632",
+    "url": "./static/media/ditu.ba7ff105.png"
+  }
+]);
